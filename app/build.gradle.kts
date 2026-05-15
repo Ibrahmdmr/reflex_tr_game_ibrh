834e3e58656d0f891c2f7ac193e9d08525ac0877
ibrahim.demir <ibrahim.demir@flo.com.tr>
2026-05-15 10:44:49 +0300

    Bump application version to 1.0.6
    - Increment `versionCode` to 6
    - Update `versionName` to 1.0.6 in the app-level build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.reflex.tr.game.ibrh"
         minSdk = 25
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
